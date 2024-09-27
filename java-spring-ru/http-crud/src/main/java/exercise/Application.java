bbc7d33282badc0e2a40c5515f656e6311d3909b
@hexlet/cli <support@hexlet.io>
2024-09-27 12:54:52 +0400

    submit java-spring-ru/http-crud

--- java-spring-ru/http-crud/src/main/java/exercise/Application.java
+++ java-spring-ru/http-crud/src/main/java/exercise/Application.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import exercise.model.Post;
